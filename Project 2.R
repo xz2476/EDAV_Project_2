eb0751a7d5cbfea97eaa1a30c45d957270114929
xiaowei zhang <xz2476@columbia.edu>
2016-02-24 00:40:52 -0500

    update on plotting floods (day by day)

--- Project 2.R
+++ Project 2.R
@@ -42,21 +42,49 @@ phi.t1 <- data.frame(cbind(lonlat,phi.vec))
 colnames(phi.t1) <- c("Longitude","Latitude","Presure")
 head(phi.t1)
 
-#matrix for period of time (eg: 01/01/50 ti 01/01/70)
-a <- which(ltime == "01/01/50")
-b <- which(ltime == "01/01/70")
+#matrix for period of time (eg: 01/12/1984 to 01/01/2016)
+a <- which(ltime == "11/30/84")
+b <- which(ltime == "02/05/16")
 phi.period <- as.vector(phi[, , a:b])
 phi.mat <- matrix(phi.period, nrow = dim(lon)*dim(lat), ncol = (b-a+1))
 phi.mat <- data.frame(cbind(lonlat, phi.mat))
 colnames(phi.mat) <- c("Longitude","Latitude",as.character(ltime[a:b]))
+ltime <- ltime[a:b]
 
-csvfile <- "phi_location(1950-1970).csv"
-write.table(na.omit(phi.mat),csvfile, row.names = FALSE, sep = ",")
+#Subset the date to the area we are interested in:
+MasterTable$Centroid.X <- as.numeric(as.character(MasterTable$Centroid.X))
+MasterTable$Centroid.Y <- as.numeric(as.character(MasterTable$Centroid.Y))
+MasterTable <- MasterTable[MasterTable$Centroid.X >= (-92.5) & MasterTable$Centroid.X <= (-75) & MasterTable$Centroid.Y >= 37.5 & MasterTable$Centroid.Y <= 47.5, ]
+MasterTable <- MasterTable[!is.na(MasterTable$Centroid.X),]
+####################################################################
 
+library(ggmap)
+library(ggplot2)
+
+#flood for purticular day:
+#take subset of a particular day:
+
+#formate the date in "mastertable" to match the date format in "phi.mat":
+MasterTable$Began <- as.character(MasterTable$Began)
+MasterTable$Began <- as.Date(MasterTable$Began,"%d-%B-%y")
+
+MasterTable$Ended <- as.character(MasterTable$Ended)
+MasterTable$Ended <- as.Date(MasterTable$Ended,"%d-%B-%y")
+
+ltime <- as.Date(ltime)
+#create subset of "mastertable" contains inforamtion of certain day:
+box <- make_bbox(lon = c(-92.5,-75),lat = c(37.5,47.5))
+map <- get_map(location = box)
 
 #plot:
-library(ggmap)
-box <- make_bbox(lon= phi.mat$Longitude,lat = phi.mat$Latitude)
-map <- get_map(location = box, source = "stamen")
-ggmap(map)
-qmplot(Longitude,Latitude,data = phi.mat)
+for(i in 1:length(ltime)){
+  if(any(MasterTable$Began <= ltime[i] & MasterTable$Ended >= ltime[i])==TRUE)
+  { 
+    df <- MasterTable[MasterTable$Began <= ltime[i] & MasterTable$Ended >= ltime[i] ,]   
+    date <-as.character(ltime[i]) 
+    x <- ggmap(map) +geom_point(data = df, aes(x = Centroid.X, y = Centroid.Y),color = "red",size = 4)+ ggtitle(date)
+    ggsave(x, device = "png",filename = date,path = "~/desktop")
+    }
+  else{
+  }
+}
